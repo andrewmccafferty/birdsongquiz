4a2ba4231049547b051ed2c9dd42f9c700b588cb
Andrew McCafferty <andymccafferty@gmail.com>
2025-01-17 10:28:08 +0000

    Remove some redundant stuff from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,5 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.aws_region
-
-  default_tags {
-    tags = {
-      hashicorp-learn = "lambda-api-gateway"
-    }
-  }
-
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
