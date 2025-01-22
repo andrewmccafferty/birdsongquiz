471280946b5b14a0d14d00460974188997fb039f
Andrew McCafferty <andymccafferty@gmail.com>
2025-01-22 16:54:05 +0000

    Remove some commented out parts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import HeadToHeadSpeciesSelector from "./HeadToHeadSpeciesSelector";
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
 
-
-
-// const onHeadToHeadSpeciesSelected = (speciesList) => {
-//     console.log(speciesList);
-// }
-
-// function App() {
-//   return <div><h1>Head-to-Head Birdsong quiz</h1>
-// <HeadToHeadSpeciesSelector onSelectionComplete={headToHeadSpeciesList => onHeadToHeadSpeciesSelected(headToHeadSpeciesList)}/> 
-//   </div>;
-// }
-
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App tab="home" />);
