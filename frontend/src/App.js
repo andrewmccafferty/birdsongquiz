3d4c0045474c3c6dc2088cb9f18b02e71898d8d8
Andrew McCafferty <andymccafferty@gmail.com>
2025-01-23 11:40:45 +0000

    Add preset species

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,25 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
+        const presetSpecies = this.decodePresetSpecies()
         this.state = {
-            headToHeadSpeciesList: []
+            headToHeadSpeciesList: presetSpecies ? presetSpecies : []
         }
-  
+    }
+
+    decodePresetSpecies = () => {
+        try {
+            const urlParams = new URLSearchParams(window.location.search)
+            const presetSpeciesBase64 = urlParams.get('presetSpecies')
+            if (!presetSpeciesBase64) {
+                return null
+            }
+            const presetSpecies = JSON.parse(atob(presetSpeciesBase64))
+            return presetSpecies
+        } catch (e) {
+            console.error("error decoding preset species", e)
+            return null
+        }
     }
 
     onHeadToHeadSpeciesSelected = headToHeadSpeciesList => {
@@ -24,17 +39,23 @@ class App extends Component {
     headToHeadLabel = () =>
         `Head to head species: ${this.state.headToHeadSpeciesList.map(species => species.Species).join(", ")}`
 
+    headToHeadSharingLink = () => `${window.location.origin}?presetSpecies=${btoa(JSON.stringify(this.state.headToHeadSpeciesList))}`
+
     render() {
         return <div><h1>Head-to-Head Birdsong quiz</h1>
-         { (!this.state.headToHeadSpeciesList || this.state.headToHeadSpeciesList.length === 0) &&
-                <HeadToHeadSpeciesSelector onSelectionComplete={headToHeadSpeciesList => this.onHeadToHeadSpeciesSelected(headToHeadSpeciesList)}/>
+            {(!this.state.headToHeadSpeciesList || this.state.headToHeadSpeciesList.length === 0) &&
+                <HeadToHeadSpeciesSelector onSelectionComplete={headToHeadSpeciesList => this.onHeadToHeadSpeciesSelected(headToHeadSpeciesList)} />
             }
-        {
-            this.state.headToHeadSpeciesList && this.state.headToHeadSpeciesList.length > 0 &&
+            {
+                this.state.headToHeadSpeciesList && this.state.headToHeadSpeciesList.length > 0 &&
                 <h2>{this.headToHeadLabel()}</h2>
-        }
-        { this.state.headToHeadSpeciesList && this.state.headToHeadSpeciesList.length > 0 &&
-                    <GameControls headToHeadSpecies={this.state.headToHeadSpeciesList}/>}
+            }
+            {this.state.headToHeadSpeciesList && this.state.headToHeadSpeciesList.length > 0 &&
+                <GameControls headToHeadSpecies={this.state.headToHeadSpeciesList} />}
+            {
+                this.state.headToHeadSpeciesList && this.state.headToHeadSpeciesList.length > 0 &&
+                <a href={this.headToHeadSharingLink()} style={{ 'margin-left': '5px' }}>permalink</a>
+            }
         </div>
     }
 }
