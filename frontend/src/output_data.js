22cb20a324034a41eb2c234ed26bef714c5f5046
Andrew McCafferty <andymccafferty@gmail.com>
2025-07-28 16:53:04 +0100

    Remove redundant file

--- frontend/src/output_data.js
+++
@@ -1,6 +0,0 @@
-const species_list = require("./species_list");
-
-console.log(JSON.stringify(species_list.map(item => ({
-    Species: item.Species,
-    ScientificName: item.ScientificName
-})), null, 2));
