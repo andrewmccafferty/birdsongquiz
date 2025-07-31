a34add70d62b90f38b8b169c41af163fffee8b7d
Andrew McCafferty <andymccafferty@gmail.com>
2025-07-31 15:13:24 +0100

    Hard-code region in API base url

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "api_base_url" {
   description = "Base URL for the deployed API Gateway stage"
-  value       = "https://${aws_apigatewayv2_api.lambda.api_id}.execute-api.${aws_apigatewayv2_api.lambda.region}.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}/"
+  value       = "https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}/"
 }
