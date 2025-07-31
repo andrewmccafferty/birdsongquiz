55778b34e219779a0fc172505eaf1d903b7a82c8
Andrew McCafferty <andymccafferty@gmail.com>
2025-07-31 16:52:36 +0100

    Run deploy frontend

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -14,6 +14,9 @@ if [ -z "${FRONTEND_BUCKET:-}" ]; then
 fi
 
 # 2. Build the app
+echo "Running npm ci"
+npm ci
+
 echo "Running build with API_ROOT=$API_ROOT..."
 API_ROOT="$API_ROOT" npm run build
 
