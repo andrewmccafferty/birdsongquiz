a22ab49691c174b884001fedc604e055d838ed81
Andrew McCafferty <andymccafferty@gmail.com>
2025-07-31 17:09:05 +0100

    Try again...

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "api_root" {
+output "api_base_url" {
   description = "Base URL for the deployed API Gateway stage"
   value       = "https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}/"
 }
