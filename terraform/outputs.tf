57c736fbeafa710b2dd87ed3391501649a883e66
Andrew McCafferty <andymccafferty@gmail.com>
2025-08-01 12:37:36 +0100

    Remove trailing slash

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "api_base_url" {
   description = "Base URL for the deployed API Gateway stage"
-  value       = "https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}/"
+  value       = "https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}"
 }
 
 output "frontend_bucket" {
