237810755cb1e8692d90dc2dbe10dba566070e86
Andrew McCafferty <andymccafferty@gmail.com>
2025-08-07 11:56:46 +0100

    Add gulls presets

--- frontend/src/presets.js
+++ frontend/src/presets.js
@@ -1,18 +1,28 @@
 const COUNTRY_TO_PRESETS_MAPPING = {
-    "GB": [{
-        "name": "Common UK garden birds",
-        "id": "common-uk-garden"
+  GB: [
+    {
+      name: "Common UK garden birds",
+      id: "common-uk-garden",
     },
     {
-        "name": "Garden Warbler vs Blackcap",
-        "id": "garden-warbler-vs-blackcap"
-    }]
-}
+      name: "Garden Warbler vs Blackcap",
+      id: "garden-warbler-vs-blackcap",
+    },
+    {
+      name: "Common UK Gulls",
+      id: "common-uk-gulls",
+    },
+    {
+      name: "All UK Gulls",
+      id: "all-uk-gulls",
+    },
+  ],
+};
 
 const presetListsForCountry = (countryCode) => {
-    const presets = COUNTRY_TO_PRESETS_MAPPING[countryCode]
-    console.log("Found preset", presets)
-    return presets
-}
+  const presets = COUNTRY_TO_PRESETS_MAPPING[countryCode];
+  console.log("Found preset", presets);
+  return presets;
+};
 
-export { presetListsForCountry }
+export { presetListsForCountry };
