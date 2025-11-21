6d38c6a5aaa360dc8acad97cd4a272591139eb4a
Andrew McCafferty <andymccafferty@gmail.com>
2025-11-21 12:08:25 +0000

    Remove unused reference

--- frontend/src/PresetSpeciesSelector.js
+++ frontend/src/PresetSpeciesSelector.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { presetListsForCountry } from "./presets";
 class PresetSpeciesSelector extends Component {
   constructor(props) {
     super(props);
