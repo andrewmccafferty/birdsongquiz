24243d361224aee64c05d88d13b6c82b268ddfc7
Andrew McCafferty <andymccafferty@gmail.com>
2025-11-24 10:45:54 +0000

    Add cache header to preset species list

--- api/handlers.js
+++ api/handlers.js
@@ -65,5 +65,5 @@ export const getSpeciesPresetLists = async (event) => {
     return response(400, {"message": "Missing required path parameter 'region'"});
   }
   console.log("Loading preset lists with region", region);
-  return response(200, {"presets": await getSpeciesPresetListsForRegion(region) });
+  return response(200, {"presets": await getSpeciesPresetListsForRegion(region) }, true);
 }
