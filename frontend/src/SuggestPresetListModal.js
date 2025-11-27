c72afb9bfcf93c5348fa007b267c705803b0a9ea
Andrew McCafferty <andymccafferty@gmail.com>
2025-11-27 15:04:24 +0000

    Add a thank you notice to the list submit

--- frontend/src/SuggestPresetListModal.js
+++ frontend/src/SuggestPresetListModal.js
@@ -6,7 +6,8 @@ class SuggestPresetListModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listName: ""
+      listName: "",
+      submitted: false,
     };
     this.handleOverlayClick = this.handleOverlayClick.bind(this);
     this.submitList = this.submitList.bind(this);
@@ -20,16 +21,17 @@ class SuggestPresetListModal extends React.Component {
 
   submitList() {
     postApi("presets/suggestion", {
-    "region": this.props.country,
-    "listName": this.state.listName,
-    "speciesList": this.props.speciesList
-    }).then((result) => {
-      console.log("Got result", result);
-      this.props.onClose();
-    }).catch(() => {
-      alert("Something went wrong submitting your list. Try again.");
+      region: this.props.country,
+      listName: this.state.listName,
+      speciesList: this.props.speciesList,
     })
-    
+      .then((result) => {
+        console.log("Got result", result);
+        this.setState({ submitted: true });
+      })
+      .catch(() => {
+        alert("Something went wrong submitting your list. Try again.");
+      });
   }
 
   render() {
@@ -41,37 +43,58 @@ class SuggestPresetListModal extends React.Component {
     return (
       <div className="modal" onClick={this.handleOverlayClick}>
         <div className="modal-box">
-          <span className="close-btn" onClick={onClose}>&times;</span>
+          <span className="close-btn" onClick={onClose}>
+            &times;
+          </span>
 
           <h2>Suggest species list</h2>
-          <p>Like this list of species? Want to suggest it as a preset for others to use?</p>
-
-          <div>Species:</div>
-          {speciesList && speciesList.length ? (
-            <ul>
-              {speciesList.map((entry, i) => (
-                <li key={entry.Species || i}>{entry.Species}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>No species available.</p>
+          {this.state.submitted && (
+            <div class="info-notice">
+              <span class="info-icon">ℹ️</span>
+              <div class="info-text">
+                Thanks for your list suggestion! It'll be reviewed by a human,
+                and should appear in the list soon.
+                <button className="action-button" onClick={this.props.onClose}>
+                  OK
+                </button>
+              </div>
+            </div>
           )}
+          {!this.state.submitted && (
+            <div>
+              <p>
+                Like this list of species? Want to suggest it as a preset for
+                others to use?
+              </p>
 
-          <div>Region: {country}</div>
+              <div>Species:</div>
+              {speciesList && speciesList.length ? (
+                <ul>
+                  {speciesList.map((entry, i) => (
+                    <li key={entry.Species || i}>{entry.Species}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No species available.</p>
+              )}
 
-          <div className="input-container">
-            <label htmlFor="list-name">Give your list a name:</label>
-            <input
-              type="text"
-              id="list-name"
-              value={listName}
-              onChange={(e) => this.setState({ listName: e.target.value })}
-            />
-          </div>
+              <div>Region: {country}</div>
 
-          <div>
-            <button onClick={this.submitList}>Submit</button>
-          </div>
+              <div className="input-container">
+                <label htmlFor="list-name">Give your list a name:</label>
+                <input
+                  type="text"
+                  id="list-name"
+                  value={listName}
+                  onChange={(e) => this.setState({ listName: e.target.value })}
+                />
+              </div>
+
+              <div>
+                <button onClick={this.submitList}>Submit</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
