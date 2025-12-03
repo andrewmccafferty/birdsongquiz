1126410059276629b0a20a7e17afc95ec50d24fe
Andrew McCafferty <andymccafferty@gmail.com>
2025-12-03 16:08:57 +0000

    Change source dir for zip in backend deploy

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,7 +5,7 @@ locals {
 data "archive_file" "lambda_birdsongquiz" {
   type = "zip"
 
-  source_dir  = "../api/dist"
+  source_dir  = "../api"
   output_path = "${path.module}/birdsongquiz_lambdas.zip"
 }
 
