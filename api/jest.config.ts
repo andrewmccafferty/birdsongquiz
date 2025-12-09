b0907d4928332d9d4ad138e026a4415f05562e60
Andrew McCafferty <andymccafferty@gmail.com>
2025-12-09 16:29:22 +0000

    Fiddle around with packages etc to get tests working with mocking etc

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -104,7 +104,8 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+   preset: 'ts-jest/presets/default-esm',
+   extensionsToTreatAsEsm: ['.ts'],
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -177,7 +178,17 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Fixes path issues with ESM
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
--- api/tests/handlers.test.ts
+++ api/tests/handlers.test.ts
@@ -1,5 +1,15 @@
+import { getRandomRecordingForSpecies } from '../src/recording';
+import { getRecording } from '../src/handlers'
+import { mockApiGatewayEvent } from './mockObjects';
+
+jest.mock('../src/recording', () => ({
+  getRandomRecordingForSpecies: jest.fn()
+}))
+
+const TEST_SPECIES = "Testus specieus"
 describe("getRecording tests", () => {
     it("Should call through to getRandomRecordingForSpecies", () => {
+        getRecording(mockApiGatewayEvent(null, { species: TEST_SPECIES }))
         
     })
 })
