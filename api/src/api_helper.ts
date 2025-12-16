6eedf20c077b582a04e370b6011246721b5aaa68
Andrew McCafferty <andymccafferty@gmail.com>
2025-12-16 11:59:43 +0000

    Try using fetch when calling APIs in backend

--- api/src/api_helper.ts
+++ api/src/api_helper.ts
@@ -1,43 +1,20 @@
-import * as http from "http"
-import * as https from "https"
-
 const getApiJsonData = async <T>(url: string): Promise<T> => {
   console.log("Calling with URL:", url)
-  // Tried to do this with fetch, but it didn't work in the Lambda
-  // for some reason.
-  return new Promise((resolve, reject) => {
-    const lib = url.startsWith("https") ? https : http
 
-    const request = lib.get(url, (response) => {
-      let data = ""
+  const response = await fetch(url)
 
-      response.on("data", (chunk) => {
-        data += chunk
-      })
+  if (!response.ok) {
+    const errorText = await response.text()
+    throw new Error(
+      `Failed to fetch recording results: status code ${response.status}, response body ${errorText}`
+    )
+  }
 
-      response.on("end", () => {
-        if (response.statusCode === 200) {
-          try {
-            resolve(JSON.parse(data))
-          } catch (error: unknown) {
-            reject(new Error(`Error parsing JSON: ${error}`))
-          }
-        } else {
-          reject(
-            new Error(
-              `Failed to fetch recording results: status code ${response.statusCode}, response body ${data}`
-            )
-          )
-        }
-      })
-    })
-
-    request.on("error", (error) => {
-      reject(new Error(`Request error: ${error.message}`))
-    })
-
-    request.end()
-  })
+  try {
+    return (await response.json()) as T
+  } catch (error: unknown) {
+    throw new Error(`Error parsing JSON: ${error}`)
+  }
 }
 
 export { getApiJsonData }
