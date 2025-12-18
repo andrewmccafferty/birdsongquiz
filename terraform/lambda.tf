c1a1ff1da6d069f4271c5aa313339519fc028429
Andrew McCafferty <andymccafferty@gmail.com>
2025-12-18 14:16:13 +0000

    Add API base URL in to notify Lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -53,7 +53,7 @@ locals {
         MAILER_SEND_API_KEY                        = var.mailer_send_api_key
         NOTIFICATIONS_FROM_EMAIL_ADDRESS           = var.notifications_from_email_address
         NOTIFICATIONS_TO_EMAIL_ADDRESS             = var.notifications_to_email_address
-        API_BASE_URL                               = https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}
+        API_BASE_URL                               = "https://${aws_apigatewayv2_api.lambda.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.name}"
       }
       role_name = "notify_preset_list_suggested_role"
     }
